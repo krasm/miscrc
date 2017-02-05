2a0f740f50ce6e45c6591b8db5d8ef7ceb7a4ec1
krasm <mkrasowski@gmail.com>
2017-02-05 22:36:40 +0000

    simplified screenrc

--- bat.sh
+++ bat.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-STATE=`cat /proc/acpi/battery/BAT0/state | sed -n 's/charging state:\s*\(.\+\)\s*/\1/p'`
-CAPACITY=`cat /proc/acpi/battery/BAT0/info | sed -n 's/design capacity:\s*\([0-9]\+\)\(.\+\)/\1/p'`
-CURRENT=`cat /proc/acpi/battery/BAT0/state | sed -n 's/remaining capacity:\s*\([0-9]\+\)\(.\+\)/\1/p'`
-
-if [ $STATE = 'discharging' ] ; then 
-    echo -n '-'
-elif [ $STATE = 'charging' ] ; then
-    echo -n '+'
-fi
-
-echo `dc -e "2 k $CURRENT $CAPACITY / 100 * p"`
-
+acpi -b | awk '{print $4;}'

a2b67eeefa672985c9ebb4e71c74451d1ab58aa0
marcin <m@krasm.net>
2020-11-25 20:46:13 +0000

    added script to install fonts

---
+++ installfonts.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+rm -rf antt-tex
+rm -f antt.zip
+rm -rf ./tmp/ 
+
+GROFF_FONT_PATH=$HOME/.fonts/groff/devps
+GS_FONT_PATH=$HOME/.fonts/gs
+
+
+create_dir() {
+    D=$1
+    echo "create directory $DIR"
+    if [ ! -d $D ]; then 
+	mkdir -p $D
+    fi
+}
+
+generate_fonts() {
+    echo "generating postscript fonts"
+    for i in `find . -name *.afm -type f`; do 
+	OUTNAME=`basename $i .afm`
+	OUTNAME=`echo $OUTNAME | tr '[:lower:]' '[:upper:]'`
+	echo "converting $i into $OUTNAME"
+	afmtodit -d /usr/share/groff/current/font/devps/DESC -e /usr/share/groff/current/font/devps/text.enc $i /usr/share/groff/current/font/devps/generate/textmap $GROFF_FONT_PATH/$OUTNAME
+    done
+    
+    echo "copying binary fonts"
+    for i in `find . -name *.pfb -type f` ; do
+	cp $i $GS_FONT_PATH/
+    done
+}
+
+# this is full font for TeX 
+SRC="https://jmn.pl/pliki/AntykwaTorunska-tex-2_08.zip"
+curl -# -o antt.zip $SRC
+
+unzip antt.zip
+
+create_dir $GROFF_FONT_PATH
+create_dir $GS_FONT_PATH
+
+generate_fonts
+
+rm -rf antt-tex/
+
+curl -# -O ftp://ftp.gust.org.pl/pub/GUST/contrib/fonts/cm-pl/plpsfont.zip
+unzip plpsfont.zip
+
+generate_fonts
+
+rm -rf plpsfont/
+
